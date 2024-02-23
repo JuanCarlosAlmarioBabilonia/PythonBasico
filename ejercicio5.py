59a50d0ef14fd12936014df8b75f2785424ccf78
JuanCarlosAlmarioBabilonia <160683492+JuanCarlosAlmarioBabilonia@users.noreply.github.com>
2024-02-23 15:20:08 -0500

    Solucion del ejercicio 5

---
+++ ejercicio5.py
@@ -0,0 +1,3 @@
+temp = float (input("Ingrese la temperatura en grados celsius que desea convertir: "))
+fahr = (temp*(9/5))+32
+print ("La temperatura en grados fahrenheit es igual a:",fahr, "grados fahrenheit")
