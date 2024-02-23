560bfb8f47717c898b3d4583df1543740673387b
JuanCarlosAlmarioBabilonia <160683492+JuanCarlosAlmarioBabilonia@users.noreply.github.com>
2024-02-23 15:56:46 -0500

    Solucion del ejercicio 8

---
+++ ejercicio8.py
@@ -0,0 +1,3 @@
+num=int(input("Ingrese el numero de la tabla de multiplicar que desea: "))
+print ("La tabla del",num,"es la siguiente:",num*1, num*2, num*3,num*4, num*5, num*6, num*7, num*8, num*9, num*10)
+    
