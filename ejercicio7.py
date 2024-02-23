ee8594d26c22648266f3751380f40df1fa18f725
JuanCarlosAlmarioBabilonia <160683492+JuanCarlosAlmarioBabilonia@users.noreply.github.com>
2024-02-23 15:33:24 -0500

    Solucion del ejercicio 7

---
+++ ejercicio7.py
@@ -0,0 +1,4 @@
+base=float (input("Ingrese la medida de la base del triangulo: "))
+altura=float (input("Ingrese la medida de la altura del triangulo: "))
+res= (base*altura)/2
+print ("El area del triangulo es: ",res)
