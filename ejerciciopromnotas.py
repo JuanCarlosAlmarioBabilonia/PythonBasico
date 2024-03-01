a1716bd3a82d25ef5bb66d54ca268127ee74556e
JuanCarlosAlmarioBabilonia <160683492+JuanCarlosAlmarioBabilonia@users.noreply.github.com>
2024-03-01 14:05:58 -0500

    ejerciciopromnotas

---
+++ ejerciciopromnotas.py
@@ -0,0 +1,6 @@
+n1=int(input("Ingrese su primera nota: "))
+n2=int(input("Ingrese su segunda nota: "))
+n3=int(input("Ingrese su tercera nota: "))
+n4=int(input("Ingrese su cuarta nota: "))
+prom=(n1+n2+n3+n4)/4
+print("El promedio de sus notas es: ",prom)
