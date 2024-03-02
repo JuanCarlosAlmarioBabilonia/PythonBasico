26868a6c5f29d939f7567a977a7c879c76423631
JuanCarlosAlmarioBabilonia <160683492+JuanCarlosAlmarioBabilonia@users.noreply.github.com>
2024-03-02 07:54:18 -0500

    ejercicioinvertirnum

--- ejercicioinvertirnum.py
+++ ejercicioinvertirnum.py
@@ -1,3 +1,6 @@
 num=int(input("Ingrese el numero de tres cifras que desea invertir: "))
-num=num[::-1]
-print(num)
+d1=num//100
+d2=(num%100)//10
+d3=num%10
+res=(d3*100)+(d2*10)+d1
+print(f"El numero invertido de {num} es {res}")
